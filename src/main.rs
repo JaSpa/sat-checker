63209584a0e783de1838c7c1035fd66f92bf29e9
Janek Spaderna <janek@spaderna.eu>
2023-05-25 05:51:38 +0200

    Seeding

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-use std::path::PathBuf;
-
 use clap::Parser;
+use color_eyre::eyre::Result;
+use rand::SeedableRng;
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 struct Args {
     /// Seed to reproduce results.
     #[arg(short, long)]
-    seed: Option<usize>,
+    seed: Option<u64>,
 
     /// CNF instance to test with.
     instance: Option<PathBuf>,
 }
 
-fn main() {
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
     let args = Args::parse();
+    let seed = match args.seed {
+        Some(seed) => seed,
+        None => gen_seed()?,
+    };
+
+    println!("Rerun with  --seed={seed}");
+
+    let _rng = rand::rngs::SmallRng::seed_from_u64(seed);
 
     println!("{args:?}");
+    Ok(())
+}
+
+fn gen_seed() -> Result<u64> {
+    let mut bytes = [0; 8];
+    getrandom::getrandom(&mut bytes)?;
+    Ok(u64::from_ne_bytes(bytes))
 }
