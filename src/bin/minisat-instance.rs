32ddb322b63b67d851bfe6718ba2526a428c7c20
Janek Spaderna <janek@spaderna.eu>
2023-05-25 05:57:06 +0200

    Multiple binaries

---
+++ src/bin/minisat-instance.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("c  If you are seeing this output on a terminal you called the `minisat-instance`");
+    println!("c  program directly, which is not intended. Use `minisat-tests` instead.");
+}
